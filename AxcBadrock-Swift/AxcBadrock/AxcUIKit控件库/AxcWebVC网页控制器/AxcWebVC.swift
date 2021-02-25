004b52c967dcffe90e8bd1c64a9db69bea9c7a16
AxcLogo <axclogo@163.com>
2021-02-26 01:00:16 +0800

    准备写webview

---
+++ AxcBadrock-Swift/AxcBadrock/AxcUIKit控件库/AxcWebVC网页控制器/AxcWebVC.swift
@@ -0,0 +1,29 @@
+//
+//  AxcWebVC.swift
+//  AxcBadrock-Swift
+//
+//  Created by 赵新 on 2021/2/26.
+//
+
+import UIKit
+
+class AxcWebVC: AxcBaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
