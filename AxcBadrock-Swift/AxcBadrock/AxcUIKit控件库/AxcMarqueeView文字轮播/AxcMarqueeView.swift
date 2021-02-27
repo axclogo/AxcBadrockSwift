4de230f8c83984b2e08c18e8043c9851953f9601
AxcLogo <axclogo@163.com>
2021-02-28 02:28:09 +0800

    准备封装文字轮播

---
+++ AxcBadrock-Swift/AxcBadrock/AxcUIKit控件库/AxcMarqueeView文字轮播/AxcMarqueeView.swift
@@ -0,0 +1,20 @@
+//
+//  AxcMarqueeView.swift
+//  AxcBadrock-Swift
+//
+//  Created by 赵新 on 2021/2/28.
+//
+
+import UIKit
+
+class AxcMarqueeView: AxcBaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
