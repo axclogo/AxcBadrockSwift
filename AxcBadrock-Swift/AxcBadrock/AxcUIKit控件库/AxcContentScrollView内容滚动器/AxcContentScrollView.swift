81104132b5ef6cf9c67a572091bd036679255d55
AxcLogo <axclogo@163.com>
2021-03-08 15:02:22 +0800

    改名为视图滚动器，不限于何种内容

---
+++ AxcBadrock-Swift/AxcBadrock/AxcUIKit控件库/AxcContentScrollView内容滚动器/AxcContentScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  AxcContentScrollView.swift
+//  AxcBadrock-Swift
+//
+//  Created by 赵新 on 2021/3/8.
+//
+
+import UIKit
+
+class AxcContentScrollView: AxcBaseControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
