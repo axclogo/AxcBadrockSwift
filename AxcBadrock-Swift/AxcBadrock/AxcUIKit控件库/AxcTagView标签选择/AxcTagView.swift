e43e7cd65ab7c898700f8f60f40defc41587a607
AxcLogo <axclogo@163.com>
2021-03-08 14:59:16 +0800

    继续做文字滚动

---
+++ AxcBadrock-Swift/AxcBadrock/AxcUIKit控件库/AxcTagView标签选择/AxcTagView.swift
@@ -0,0 +1,20 @@
+//
+//  AxcTagView.swift
+//  AxcBadrock-Swift
+//
+//  Created by 赵新 on 2021/3/8.
+//
+
+import UIKit
+
+class AxcTagView: AxcBaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
